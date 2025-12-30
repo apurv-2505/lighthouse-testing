2093d5a3fb66c94a83abd747fbd25262795f4fe7
apurv-edition <apurv.editiongroup.com>
2025-12-30 23:03:33 +0530

    Lighthouse testing

---
+++ src/About.jsx
@@ -0,0 +1,13 @@
+// src/About.jsx
+import React from "react";
+
+function About() {
+  return (
+    <div>
+      <h1>About Page</h1>
+      <p>This is a dummy About Page for our app.</p>
+    </div>
+  );
+}
+
+export default About;
